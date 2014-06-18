92119a03e19869e4f54b1ea38fd9d0160e750bc5
Helen Hood <helen.c.hood@gmail.com>
2014-06-18 16:24:35 -0400

    Add create action in QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,15 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    end
+  end
 
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
   end
 end
