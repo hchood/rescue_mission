770d77aebee338c5bd1fe1363431076c3ba689eb
Helen Hood <helen.c.hood@gmail.com>
2014-06-18 16:01:28 -0400

    Add happy path spec for add question

---
+++ spec/features/user_posts_a_question_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature "user posts a question", %Q{
+  As a user
+  I want to post a question
+  So that I can receive help from others
+} do
+
+# Acceptance Criteria:
+# - I must provide a title that is at least 40 characters long
+# - I must provide a description that is at least 150 characters long
+# - I must be presented with errors if I fill out the form incorrectly
+
+  scenario "user provides required attributes" do
+    attrs = {
+      title: "I have a really long question about Rails",
+      description: %Q{
+        Flannel you probably haven't heard of them Helvetica
+        PBR&B VHS. Wes Anderson kale chips authentic pour-over
+        four loko, plaid pug meggings umami crucifix Shoreditch
+        selvage Portland cray. PBR pug cliche tote bag, hashtag
+        hoodie chambray lomo fanny pack Pitchfork deep v butcher.
+        Selfies lomo shabby chic literally, hashtag fap brunch.
+        Cliche keffiyeh ethical post-ironic, 3 wolf moon messenger
+        bag Godard stumptown High Life pop-up YOLO letterpress Pinterest cornhole.
+      }
+     }
+
+    prev_count = Question.count
+
+    visit new_question_path
+
+    fill_in 'Title', with: attrs[:title]
+    fill_in 'Description', with: attrs[:description]
+    click_on 'Create Post'
+
+    expect(page).to have_content "Success!"
+    expect(page).to have_content attrs[:title]
+    expect(page).to have_content attrs[:description]
+    expect(Question.count).to eq prev_count + 1
+  end
+
+  scenario "missing required attributes"
+
+  scenario "title and description too short"
+end
