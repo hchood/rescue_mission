8f9a497ac9a738d17d870f2bc42d377eb1e13722
Helen Hood <helen.c.hood@gmail.com>
2014-06-19 15:53:33 -0400

    Add find_or_create_by_auth_hash method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
+  validates :provider, presence: true
+  validates :uid, presence: true
 
+  def self.find_or_create_from_auth_hash(auth_hash)
+    user = self.where(
+      provider: auth_hash[:provider],
+      uid: auth_hash[:uid]).first
+
+    if !user
+      attrs = {
+        provider: auth_hash[:provider],
+        uid: auth_hash[:uid],
+        email: auth_hash[:info][:email]
+      }
+      user = User.create(attrs)
+    end
+
+    user
+  end
 end
