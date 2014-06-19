1d0f4a900bbe78d04a2cc50cbc82a14250384cfa
Helen Hood <helen.c.hood@gmail.com>
2014-06-19 10:59:57 -0400

    Add edit & update actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,21 @@ class QuestionsController < ApplicationController
     @answer = Answer.new
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      flash[:notice] = "Success!"
+      redirect_to @question
+    else
+      flash[:notice] = "Error!"
+      render :edit
+    end
+  end
+
   def create
     @question = Question.new(question_params)
     if @question.save
