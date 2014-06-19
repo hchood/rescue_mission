f2b9d1a069a269d097bbd7b8f0cb3676b95ee2df
Helen Hood <helen.c.hood@gmail.com>
2014-06-19 10:41:47 -0400

    Add associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ActiveRecord::Base
+  belongs_to :question
+
   validates :description,
     presence: true,
     length: { minimum: 50 }
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many :answers
+
   validates :title,
     presence: true,
     length: { minimum: 40 }
