2f29025b2cdb0b3b398bf768f562006d276dd57a
Helen Hood <helen.c.hood@gmail.com>
2014-06-19 15:23:13 -0400

    Add callback route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :answers, only: :create
   end
 
+  get '/auth/:provider/callback', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
