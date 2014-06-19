d014a628859c85c48dfcbb0998851018e3a689c9
Helen Hood <helen.c.hood@gmail.com>
2014-06-19 10:29:27 -0400

    Add route to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:new, :create, :index]
+  resources :questions, only: [:new, :create, :index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
