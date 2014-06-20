75a56931ec2ca0360db1919c00a238fe9ab8b17a
Helen Hood <helen.c.hood@gmail.com>
2014-06-20 15:28:40 -0400

    Add signout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
