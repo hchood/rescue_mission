879ab4820571090ee2ebbd9b37b19dd54e1dcfa4
Helen Hood <helen.c.hood@gmail.com>
2014-06-20 15:58:17 -0400

    Add author_id to questions table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619193426) do
+ActiveRecord::Schema.define(version: 20140620195621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140619193426) do
     t.text     "description", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id",     null: false
   end
 
   create_table "users", force: true do |t|
