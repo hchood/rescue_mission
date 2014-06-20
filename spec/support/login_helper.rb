b0deb153656c160a48b7cbdb8021f967a7289c01
Helen Hood <helen.c.hood@gmail.com>
2014-06-20 15:21:30 -0400

    Create set_omniauth method

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,4 +1,16 @@
 module LoginHelper
   def set_omniauth(user)
+    # tell OmniAuth to redirect back to callback path immediately
+    OmniAuth.config.test_mode = true
+
+    # create a 'fake' auth_hash
+    auth_hash = {
+      provider: user.provider,
+      uid: user.uid,
+      email: user.email
+    }
+
+    # create the mock
+    OmniAuth.config.add_mock(:github, auth_hash)
   end
 end
